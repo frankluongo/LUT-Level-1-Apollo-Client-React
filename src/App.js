64695ec4cf7c2a40d34914d60e9da9445b3394d6
Frank Luongo <frankluongo92@gmail.com>
2018-11-12 19:56:56 -0500

    03: Writing Our First GraphQL Query

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from "react-apollo";
-import logo from './logo.svg';
+import gql from 'graphql-tag';
 import './App.css';
 
 const client = new ApolloClient({
   uri: 'https://api-useast.graphcms.com/v1/cjodhanpb78xp01dg0edo0qkt/master'
 });
+
+const testQuery = gql`
+  {
+    posts {
+      id
+      title
+      body
+    }
+  }
+`;
+
+client.query({
+  query: testQuery
+}).then(res => console.log(res));
 
 class App extends Component {
   render() {
@@ -14,18 +28,9 @@ class App extends Component {
       <ApolloProvider client={client}>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hey, demons, it's me, ya boy.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
       </ApolloProvider>
